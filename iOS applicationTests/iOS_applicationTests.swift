a9fe616eab03f0c6678e3d9cb4254f48b73fd9ea
Вова Петров <vladimir@MacBook-Pro-Vova.local>
2018-03-15 19:25:05 +0500

    Initial Commit

---
+++ iOS applicationTests/iOS_applicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_applicationTests.swift
+//  iOS applicationTests
+//
+//  Created by Вова Петров on 15.03.18.
+//  Copyright © 2018 Вова Петров. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_application
+
+class iOS_applicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
